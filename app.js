1390d9f1eccd0b5a275b4636c90a9f7329830a0c
Alexander <alexander.moreno@utp.edu.co>
2024-01-05 23:23:37 -0500

    Changes

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-// const express = require('express') // require -> commonJS
+const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
-// const cors = require('cors')
-const { app } = require('./util/cors_')
+const cors = require('cors')
+// const { app } = require('./util/cors_')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
-// const app = express()
-// app.use(express.json())
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     const ACCEPTED_ORIGINS = [
-//       'http://localhost:8080',
-//       'http://localhost:1234',
-//       'https://movies.com',
-//       'https://midu.dev'
-//     ]
+const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
 
-//     if (ACCEPTED_ORIGINS.includes(origin)) {
-//       return callback(null, true)
-//     }
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
 
-//     if (!origin) {
-//       return callback(null, true)
-//     }
+    if (!origin) {
+      return callback(null, true)
+    }
 
-//     return callback(new Error('Not allowed by CORS'))
-//   }
-// }))
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 
 // métodos normales: GET/HEAD/POST
